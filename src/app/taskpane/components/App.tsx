81abffab7bd446a5c26e833264cd95c51b1febbe
Kazuki Inamura <kzinmr109@gmail.com>
2023-07-27 02:48:40 +0900

    make taskpane/page SSR

--- src/app/taskpane/components/App.tsx
+++ src/app/taskpane/components/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+'use client'
 import Chat from './Chat';
+import React, { useEffect } from "react";
+
+let isOfficeInitialized = false;
 
 export type AppProps = {
     title: string;
-    isOfficeInitialized: boolean;
 };
 
 const App: React.FC<AppProps> = (props) => {
-    if (!props.isOfficeInitialized) {
-        // Progress bar to indicate page load
-        console.log("Office not initialized")
-    }
-    return (
-      <div className="flex flex-col h-full">
-        <div className="flex-grow">
-          <main>
-            <Chat />
-          </main>
-        </div>
+
+  useEffect(() => {
+    Office.onReady(() => {
+      isOfficeInitialized = true;
+    });
+  }, []);
+
+  if (!isOfficeInitialized) {
+      // Progress bar to indicate page load
+      console.log("Office not initialized")
+  }
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-grow">
+        <main>
+          <Chat />
+        </main>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/app/taskpane/page.tsx
+++ src/app/taskpane/page.tsx
@@ -1,17 +1,10 @@
-'use client'
 import App from "./components/App";
-import React, { useEffect } from "react";
+import React from "react";
 
-let isOfficeInitialized = false;
 const title = "Ask Word Add-in";
 
 export default function TaskPane() {
-  useEffect(() => {
-    Office.onReady(() => {
-      isOfficeInitialized = true;
-    });
-  }, []);
   return (
-    <App title={title} isOfficeInitialized={isOfficeInitialized} />
+    <App title={title} />
   )
 }
