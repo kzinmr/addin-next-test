fd5ba8cd6ce687df30d4e0177b46d84af24ed708
Kazuki Inamura <kzinmr109@gmail.com>
2023-07-29 02:25:15 +0900

    fix: path segment misspelling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <p>
-        View <Link href="/blog">all posts</Link>
+        <Link href="/">Return Home</Link>
       </p>
     </div>
   )
